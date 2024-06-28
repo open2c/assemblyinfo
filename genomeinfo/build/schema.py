2d5d9317936d086e8a9a5308a1b83c1120a298e4
alejandrogzi <jose.gonzalesdezavala1@unmsm.edu.pe>
2024-06-28 10:47:00 -0500

    genomeinfo v.0.1.0-devel

---
+++ genomeinfo/build/schema.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+NCBI = "https://ftp.ncbi.nlm.nih.gov/genomes/all"
+
+ASSEMBLY_BLACKLIST = [
+    "T2T-CHM13v0.7",
+    "NCBI33",
+    "NCBI34",
+    "MGSCv3",
+    "canfam4",
+    "Release_6_plus_MT",
+]
+REFSEQ_BLACKLIST = [
+    "GCF_000001215.2",
+    "GCF_000001215.2",
+    "GCF_000002285.2",
+    "GCF_000002315.2",
+    "GCF_000002315.6",
+    "GCF_000002035.3",
+]
+
+MAP_SPECIES_NAME = {
+    "homo_sapiens": "human",
+    "mus_musculus": "mouse",
+    "canis_lupus_familiaris": "dog",
+    "caenorhabditis_elegans": "celegans",
+    "drosophila_melanogaster": "fruitfly",
+    "danio_rerio": "zebrafish",
+    "gallus_gallus": "chicken",
+    "bos_taurus": "cow",
+}
+
+HUMAN_ASSEMBLY_MAP = {
+    "GRCh37": "hg19",
+    "GRCh38": "hg38",
+    "T2T-CHM13": "hs1",
+    "NCBI34": "hg16",
+    "NCBI35": "hg17",
+    "NCBI36": "hg18",
+}
+MOUSE_ASSEMBLY_MAP = {
+    "GRCm38": "mm10",
+    "GRCm39": "mm39",
+    "MGSCv37": "mm9",
+    "MGSCv36": "mm8",
+    "MGSCv35": "mm7",
+    "MGSCv34": "mm6",
+}
+DOG_ASSEMBLY_MAP = {
+    "UU_Cfam_GSD_1.0": "canFam4",
+    "CanFam2.0": "canFam2",
+    "CanFam3.1": "canFam3",
+    "Dog10K_Boxer_Tasha": "canFam6",
+    "ROS_Cfam_1.0": "ROS_Cfam_1.0",
+    "UMICH_Zoey_3.1": "canFam5",
+}
+CELEGANS_ASSEMBLY_MAP = {
+    "WS144": np.NaN,
+    "WBcel215": np.NaN,
+    "WBcel235": "ce11",
+    "WS190": "ce6",
+    "WS195": np.NaN,
+    "WS195": np.NaN,
+    "ASM2820141v1": np.NaN,
+}
+DROSOPHILA_ASSEMBLY_MAP = {
+    "Release_5": "dm3",
+    "Release_5": "dm3",
+    "Release_6": "dm6",
+    "Release_6_plus_ISO1_MT": "dm6",
+    "Release_6_plus_MT": "dm6",
+}
+DANIO_ASSEMBLY_MAP = {
+    "Zv8": "danRer6",
+    "Zv9": "danRer7",
+    "GRCz10": "danRer10",
+    "GRCz11": "danRer11",
+    "Zv7": "danRer5",
+    "ASM3317019v1": np.NaN,
+    "ASM3317019v2": np.NaN,
+}
+CHICKEN_ASSEMBLY_MAP = {
+    "bGalGal1.mat.broiler.GRCg7b": "galGal7",
+    "Gallus_gallus-2.1": "galGal3",
+    "Gallus_gallus-4.0": "galGal4",
+    "Gallus_gallus-5.0": "galGal5",
+    "GRCg6": np.NaN,
+    "GRCg6a": "galGal6",
+}
+COW_ASSEMBLY_MAP = {
+    "ARS-UCD1.1": np.NaN,
+    "ARS-UCD1.2": "bosTau9",
+    "ARS-UCD1.3": np.NaN,
+    "ARS-UCD2.0": np.NaN,
+}
+ASSEMBLY_MAP = (
+    HUMAN_ASSEMBLY_MAP
+    | MOUSE_ASSEMBLY_MAP
+    | DOG_ASSEMBLY_MAP
+    | CELEGANS_ASSEMBLY_MAP
+    | DROSOPHILA_ASSEMBLY_MAP
+    | DANIO_ASSEMBLY_MAP
+    | CHICKEN_ASSEMBLY_MAP
+    | COW_ASSEMBLY_MAP
+)
---
+++ genomeinfo/core/chrom.py
@@ -0,0 +1,253 @@
+import pandas as pd
+from typing import List, Optional
+
+__all__ = [
+    "filter_chromosome_data",
+    "get_chromnames",
+    "get_chromsizes",
+    "get_chrom_eq",
+    "get_seqinfo",
+]
+
+
+def filter_chromosome_data(
+    cls,
+    assembly: str,
+    roles: Optional[List[str]] = None,
+    units: Optional[List[str]] = None,
+    length: Optional[str] = None,
+) -> pd.DataFrame:
+    """
+    Filters the chromosome data based on the provided parameters.
+
+    Parameters
+    ----------
+    assembly : str
+        The assembly name to filter by.
+    roles : Optional[List[str]]
+        The roles to filter by.
+    units : Optional[List[str]]
+        The units to filter by.
+    length : Optional[str]
+        The length condition to filter by (e.g., '> 1000').
+
+    Returns
+    -------
+    pd.DataFrame
+        The filtered chromosome data.
+
+    Raises
+    ------
+    ValueError
+        If the assembly is not found in the database.
+    """
+    if assembly in cls._data["assembly"].tolist():
+        group = "assembly"
+    elif assembly in cls._data["assembly_ucsc"].dropna().tolist():
+        group = "assembly_ucsc"
+    else:
+        raise ValueError(f"{assembly} not in database!")
+
+    q1 = f'{group} == "{assembly}" and version == "latest"'
+    q2 = ""
+
+    if length:
+        q2 += f"length {length}"
+
+    if roles:
+        if len(q2) > 0:
+            q2 += f" and role.isin({roles})"
+        else:
+            q2 += f"role.isin({roles})"
+
+    if units:
+        if len(q2) > 0:
+            q2 += f"  and unit.isin({units})"
+        else:
+            q2 += f"unit.isin({units})"
+
+    if len(q2) > 0:
+        mask = (
+            pd.DataFrame.from_records(cls._data.query(q1).seqinfo.iloc[0])
+            .query(q2)
+            .convert_dtypes()
+        )
+    else:
+        mask = pd.DataFrame.from_records(
+            cls._data.query(q1).seqinfo.iloc[0]
+        ).convert_dtypes()
+
+    return mask
+
+
+def get_chromnames(
+    cls,
+    assembly: str,
+    provider: Optional[str] = None,
+    roles: Optional[List[str]] = None,
+    units: Optional[List[str]] = None,
+    length: Optional[str] = None,
+) -> List[str]:
+    """
+    Returns the chromosome names for the specified assembly.
+
+    Parameters
+    ----------
+    assembly : str
+        The assembly name to filter by.
+    provider : Optional[str]
+        The provider to filter by ('ucsc', 'genbank', 'refseq', 'ncbi').
+    roles : Optional[List[str]]
+        The roles to filter by.
+    units : Optional[List[str]]
+        The units to filter by.
+    length : Optional[str]
+        The length condition to filter by (e.g., '> 1000').
+
+    Returns
+    -------
+    List[str]
+        A list of chromosome names.
+
+    Raises
+    ------
+    ValueError
+        If the provider is not valid.
+    """
+    if not provider or provider == "ucsc":
+        colname = "name"
+    elif provider in ["genbank", "refseq", "ncbi"]:
+        colname = provider
+    else:
+        raise ValueError(f"{provider} is not a valid provider!")
+
+    return cls.filter_chromosome_data(assembly, roles, units, length)[colname].tolist()
+
+
+def get_chromsizes(
+    cls,
+    assembly: str,
+    provider: Optional[str] = None,
+    roles: Optional[List[str]] = None,
+    units: Optional[List[str]] = None,
+    length: Optional[str] = None,
+) -> pd.Series:
+    """
+    Returns the chromosome sizes for the specified assembly.
+
+    Parameters
+    ----------
+    assembly : str
+        The assembly name to filter by.
+    provider : Optional[str]
+        The provider to filter by ('ucsc', 'genbank', 'refseq', 'ncbi').
+    roles : Optional[List[str]]
+        The roles to filter by.
+    units : Optional[List[str]]
+        The units to filter by.
+    length : Optional[str]
+        The length condition to filter by (e.g., '> 1000').
+
+    Returns
+    -------
+    pd.Series
+        A series with chromosome sizes.
+
+    Raises
+    ------
+    ValueError
+        If the provider is not valid.
+    """
+    if not provider or provider == "ucsc":
+        colname = "name"
+    elif provider in ["genbank", "refseq", "ncbi"]:
+        colname = provider
+    else:
+        raise ValueError(f"{provider} is not a valid provider!")
+
+    df = cls.filter_chromosome_data(assembly, roles, units, length)
+    return df.set_index(colname)["length"]
+
+
+def get_chrom_eq(
+    cls,
+    assembly: str,
+    providers: Optional[List[str]] = None,
+    roles: Optional[List[str]] = None,
+    units: Optional[List[str]] = None,
+    length: Optional[str] = None,
+) -> pd.DataFrame:
+    """
+    Returns the chromosome equivalence for the specified assembly.
+
+    Parameters
+    ----------
+    assembly : str
+        The assembly name to filter by.
+    providers : Optional[List[str]]
+        The providers to filter by (e.g., 'ucsc', 'genbank', 'refseq', 'ncbi').
+    roles : Optional[List[str]]
+        The roles to filter by.
+    units : Optional[List[str]]
+        The units to filter by.
+    length : Optional[str]
+        The length condition to filter by (e.g., '> 1000').
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame with chromosome equivalence across different providers.
+
+    Raises
+    ------
+    ValueError
+        If the provider is not valid.
+    """
+    if not providers:
+        providers = ["name", "ncbi", "genbank", "refseq"]
+    else:
+        if "ucsc" in providers:
+            providers = [p if p != "ucsc" else "name" for p in providers]
+
+    return cls.filter_chromosome_data(assembly, roles, units, length)[providers]
+
+
+def get_seqinfo(cls, assembly: str) -> pd.DataFrame:
+    """
+    Returns the sequence information for the specified assembly.
+
+    Parameters
+    ----------
+    assembly : str
+        The assembly name to filter by.
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame with sequence information.
+    """
+    if assembly in cls._data["assembly"].tolist():
+        group = "assembly"
+    elif assembly in cls._data["assembly_ucsc"].dropna().tolist():
+        group = "assembly_ucsc"
+    elif assembly in cls._data["patch"].dropna().tolist():
+        group = "patch"
+    else:
+        raise ValueError(f"{assembly} not in database!")
+
+    q1 = f'{group} == "{assembly}"'
+    q2 = 'version == "latest"'
+
+    local_db = cls._data.query(q1)
+    if len(local_db) > 1:
+        return (
+            pd.DataFrame.from_records(local_db.query(q2).seqinfo.iloc[0])
+            .convert_dtypes()
+            .set_index("name")
+        )
+    else:
+        return (
+            pd.DataFrame.from_records(local_db.seqinfo.iloc[0])
+            .convert_dtypes()
+            .set_index("name")
+        )
